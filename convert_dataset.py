c68357718f97441b6af6b476d333e410829a4d74
Zein Shaheen <zeinshahine@gmail.com>
2020-03-16 23:11:04 +0300

    training seqGAN with generator regularization

--- convert_dataset.py
+++ convert_dataset.py
@@ -14,7 +14,7 @@ tokens=set(alltext.split())
 
 # dictionary
 dictionary={token:str(i+2) for i,token in enumerate(tokens)}
-dictionary['<pad>']=1
+dictionary['<end>']=1
 dictionary['<start>']=0
 pickle.dump(dictionary,open('dataset/dictionary.pic','wb'))
 print('len dictionary',len(dictionary))
@@ -22,7 +22,7 @@ print('len dictionary',len(dictionary))
 maxlen=40
 def getVector(sentence,dictionary,maxlen):
     sent2vec=[dictionary[token] for token in sentence.split()]
-    return sent2vec+['0']*(maxlen-len(sent2vec))
+    return ['0']+sent2vec+['1']*(maxlen-len(sent2vec)-1)
 def checkLen(sent, maxlen):
     if sent.startswith('об этом'): return False
     return len(sent.split())<=maxlen
